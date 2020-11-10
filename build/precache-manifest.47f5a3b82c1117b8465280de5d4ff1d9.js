a524ffe3c7301a1e70f8ce4e8c9e0c22bf79872e
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-10 15:33:21 +0900

    file edit

--- build/precache-manifest.47f5a3b82c1117b8465280de5d4ff1d9.js
+++ build/precache-manifest.47f5a3b82c1117b8465280de5d4ff1d9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cc797640c05ba3b66e9732b3d83b2c6e",
+    "revision": "3a239ef93ebe47a492a030e8c89f0c5f",
     "url": "/react_movie_app_api/index.html"
   },
   {
-    "revision": "0b7c9b12824ec10d65a3",
-    "url": "/react_movie_app_api/static/css/main.f3b25689.chunk.css"
+    "revision": "476c471025e14efc6696",
+    "url": "/react_movie_app_api/static/css/main.bca177b8.chunk.css"
   },
   {
     "revision": "8fc98eddfbf39b2d8fb2",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_movie_app_api/static/js/2.e9b5ce32.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0b7c9b12824ec10d65a3",
+    "revision": "476c471025e14efc6696",
     "url": "/react_movie_app_api/static/js/main.b7b927c8.chunk.js"
   },
   {
